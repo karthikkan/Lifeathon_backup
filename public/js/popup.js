11ee81b2e4366451ed5969d08a4a1da5becdde75
karthikkan <64649142+karthikkan@users.noreply.github.com>
2021-09-17 22:58:18 +0530

    new

---
+++ public/js/popup.js
@@ -0,0 +1,92 @@
+var popup = document.getElementById("myPopup");
+var pop1 = document.getElementById("myPopup1");
+var pop2 = document.getElementById("myPopup2");
+var pop3 = document.getElementById("myPopup3");
+var pop4 = document.getElementById("myPopup4");
+var pop5 = document.getElementById("myPopup5");
+var pop6 = document.getElementById("myPopup6");
+var pop7 = document.getElementById("myPopup7");
+var pop8 = document.getElementById("myPopup8");
+var pop9 = document.getElementById("myPopup9");
+var pop10 = document.getElementById("myPopup10");
+var blur = document.getElementById("table");
+var head = document.getElementById("blurr-head");
+var foot = document.getElementById("foot");
+var html = document.querySelector("body");
+function myFunction() {
+    popup.classList.toggle("show");
+    blur.classList.toggle("blurr");
+    head.classList.toggle("blurr");
+    foot.classList.toggle("blurr");
+    html.classList.toggle("html-static");
+  }
+  function myFunction1() {
+    pop1.classList.toggle("show");
+    blur.classList.toggle("blurr");
+    head.classList.toggle("blurr");
+    foot.classList.toggle("blurr");
+    html.classList.toggle("html-static");
+  }
+  function myFunction2() {
+    pop2.classList.toggle("show");
+    blur.classList.toggle("blurr");
+    head.classList.toggle("blurr");
+    foot.classList.toggle("blurr");
+    html.classList.toggle("html-static");
+  }
+  function myFunction3(){
+    pop3.classList.toggle("show");
+    blur.classList.toggle("blurr");
+    head.classList.toggle("blurr");
+    foot.classList.toggle("blurr");
+    html.classList.toggle("html-static");
+  }
+  function myFunction4(){
+    pop4.classList.toggle("show");
+    blur.classList.toggle("blurr");
+    head.classList.toggle("blurr");
+    foot.classList.toggle("blurr");
+    html.classList.toggle("html-static");
+  }
+  function myFunction5(){
+    pop5.classList.toggle("show");
+    blur.classList.toggle("blurr");
+    head.classList.toggle("blurr");
+    foot.classList.toggle("blurr");
+    html.classList.toggle("html-static");
+  }
+  function myFunction6(){
+    pop6.classList.toggle("show");
+    blur.classList.toggle("blurr");
+    head.classList.toggle("blurr");
+    foot.classList.toggle("blurr");
+    html.classList.toggle("html-static");
+  }
+  function myFunction7(){
+    pop7.classList.toggle("show");
+    blur.classList.toggle("blurr");
+    head.classList.toggle("blurr");
+    foot.classList.toggle("blurr");
+    html.classList.toggle("html-static");
+  }
+  function myFunction8(){
+    pop8.classList.toggle("show");
+    blur.classList.toggle("blurr");
+    head.classList.toggle("blurr");
+    foot.classList.toggle("blurr");
+    html.classList.toggle("html-static");
+  }
+  function myFunction9(){
+    pop9.classList.toggle("show");
+    blur.classList.toggle("blurr");
+    head.classList.toggle("blurr");
+    foot.classList.toggle("blurr");
+    html.classList.toggle("html-static");
+  }
+  function myFunction10(){
+    pop10.classList.toggle("show");
+    blur.classList.toggle("blurr");
+    head.classList.toggle("blurr");
+    foot.classList.toggle("blurr");
+    html.classList.toggle("html-static");
+  }
